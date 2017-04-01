dbff29a2d56baf1c1aba2183697a9bba087141d5
Julian Wergieluk <julian@wergieluk.com>
2017-04-01 19:52:22 +0200

    Add package sourcing from requirements.tx

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import subprocess
 
 
 with open('README.md') as f:
@@ -7,17 +8,25 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
+with open('requirements.txt') as f:
+    requirements = [p.strip().split('=')[0] for p in f.readlines() if p[0] != '-']
+
+
+def get_development_version():
+    git_output = subprocess.run(['git', 'rev-list', '--count', 'master'], stdout=subprocess.PIPE)
+    return '0.0.%s' % git_output.stdout.decode('utf-8').strip()
+
+
 setup(
     name='signaldb',
-    version='0.0.2',
+    version=get_development_version(),
     description='A market data system for financial time-series',
     long_description=readme,
     author='Julian Wergieluk',
     author_email='julian@wergieluk.com',
     url='http://www.wergieluk.com',
     license=license,
-    install_requires=['appdirs', 'click', 'packaging', 'pymongo', 'pyparsing',
-                      'python-dateutil', 'pytz', 'six', 'tonyg-rfc3339', 'finstruments'],
+    install_requires=requirements,
     packages=find_packages(),
     classifiers=[
         'Development Status :: 4 - Beta',
